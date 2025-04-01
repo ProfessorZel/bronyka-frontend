d1a31f3efc705dfbf2f82402d3313d42e1d7b07a
Egor Mukhaev <egormuhaev@yandex.ru>
2025-04-01 10:42:01 +0300

    (add) Add home icon and title

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,17 +1,21 @@
-import { useSessionUser } from "@/features/auth";
-import { Button } from "antd";
-import { CgProfile } from "react-icons/cg";
-import { MdAdminPanelSettings } from "react-icons/md";
-import { NavLink, useNavigate } from "react-router";
+import { useSessionUser } from '@/features/auth';
+import { Button } from 'antd';
+import { CgProfile } from 'react-icons/cg';
+import { IoHomeSharp } from 'react-icons/io5';
+import { MdAdminPanelSettings } from 'react-icons/md';
+import { NavLink, useLocation, useNavigate } from 'react-router';
 
 export function Header() {
   const nav = useNavigate();
   const userData = useSessionUser();
+  const { pathname } = useLocation();
+
+  const isHomePage = pathname === '/';
 
   const isAdmin = userData && userData.is_superuser;
 
-  const handleAdminPanel = () => nav("/admin/users");
-  const handleUserProfile = () => nav("/profile");
+  const handleAdminPanel = () => nav('/admin/users');
+  const handleUserProfile = () => nav('/profile');
 
   const adminPanelNavLink = isAdmin ? (
     <Button
@@ -29,10 +33,13 @@ export function Header() {
       style={{ padding: 10 }}
     >
       <NavLink
+        title="К списку рабочих мест"
         to="/"
         className="text-black text-xl md:text-2xl text-center md:text-left"
       >
-        Рабочие места
+        <div className="flex flex-row justify-start items-center gap-2">
+          {!isHomePage ? <IoHomeSharp /> : ''} Рабочие места
+        </div>
       </NavLink>
       <div className="flex flex-row gap-3 md:gap-5 items-center">
         {adminPanelNavLink}
