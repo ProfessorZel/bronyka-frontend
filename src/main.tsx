55ebdf252ddab9ed85f40d2a599e5bcb1f35951c
Egor Mukhaev <egormuhaev@yandex.ru>
2025-04-06 15:13:38 +0300

    (add) use hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { createRoot } from 'react-dom/client';
-import { MemoryRouter } from 'react-router';
+import { HashRouter } from 'react-router';
 import { App } from './app/App';
 import './app/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MemoryRouter>
+    <HashRouter>
       <App />
-    </MemoryRouter>
+    </HashRouter>
   </StrictMode>,
 );
